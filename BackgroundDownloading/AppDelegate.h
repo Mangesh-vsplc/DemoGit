6665392b06ae8398c7600fd7493b7abe3b3b46a4
Mangesh <mangesh20@gmail.com>
2015-11-18 12:15:16 +0530

    Initial Commit

---
+++ BackgroundDownloading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackgroundDownloading
+//
+//  Created by Mangesh on 18/11/15.
+//  Copyright © 2015 Mangesh  Tekale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackgroundDownloading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackgroundDownloading
+//
+//  Created by Mangesh on 18/11/15.
+//  Copyright © 2015 Mangesh  Tekale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
