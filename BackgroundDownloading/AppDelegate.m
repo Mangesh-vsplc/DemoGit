85b2b1a9ff7b03ceb2842602a129f6eb3b818704
Mangesh <mangesh20@gmail.com>
2015-11-19 16:31:28 +0530

    Initial commit

--- BackgroundDownloading/AppDelegate.m
+++ BackgroundDownloading/AppDelegate.m
@@ -17,9 +17,27 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    UIUserNotificationType types = UIUserNotificationTypeBadge |
+    UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    
+    UIUserNotificationSettings *mySettings =
+    [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    
+    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+
+    UILocalNotification *localNotif =
+    [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
+    if (localNotif) {
+        NSLog(@"Recieved Notification %@",localNotif);
+    }
+    
     return YES;
 }
 
+- (void)application:(UIApplication *)app didReceiveLocalNotification:(UILocalNotification *)notif {
+    // Handle the notificaton when the app is running
+    NSLog(@"Recieved Notification %@",notif);
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -28,6 +46,15 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+- (void)application:(UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)())completionHandler  {
+    NSLog(@"completionHandler");
+    completionHandler(UIBackgroundFetchResultNewData);
+    UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+    localNotification.alertBody = @"Download COmplete";
+    localNotification.fireDate = [NSDate date];
+    localNotification.alertTitle = @"Alert";
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];    
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
--- BackgroundDownloading/ViewController.m
+++ BackgroundDownloading/ViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "ViewController.h"
+static NSString *sessionIdentifier = @"com.vatsa.backgroundtask";
 
-@interface ViewController ()
-
+@interface ViewController () <NSURLSessionDelegate, NSURLSessionDownloadDelegate>
+@property (strong, nonatomic) IBOutlet UIButton *btnStart;
+@property (strong, nonatomic) IBOutlet UILabel *lblMessage;
+@property (strong, nonatomic) NSURLSessionConfiguration *sessionConfiguration;
+@property (strong, nonatomic) IBOutlet UIProgressView *progressView;
+@property (strong, nonatomic) IBOutlet UIImageView *imgView;
 @end
 
 @implementation ViewController
@@ -22,6 +27,41 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)startButtonTapped:(UIButton*)sender {
+    [sender setUserInteractionEnabled:NO];
+    self.sessionConfiguration = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:sessionIdentifier];
+    [self.sessionConfiguration setSessionSendsLaunchEvents:YES];
+    [self.sessionConfiguration setDiscretionary:YES];
+    self.sessionConfiguration.allowsCellularAccess = YES;
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:self.sessionConfiguration delegate:self delegateQueue:nil];
+
+    NSURL *url = [NSURL URLWithString:@"https://upload.wikimedia.org/wikipedia/commons/3/3b/Colored-Pencils.jpg"];
+    NSURLSessionDownloadTask *dataTask = [session downloadTaskWithURL:url];
+    [dataTask resume];
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
+didFinishDownloadingToURL:(NSURL *)location {
+    NSLog(@"%@",location);
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        self.progressView.progress = 1.0;
+        NSData *data = [NSData dataWithContentsOfURL:location];
+        [self.imgView setImage:[UIImage imageWithData:data]];
+        
+    });
+
+}
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
+      didWriteData:(int64_t)bytesWritten
+ totalBytesWritten:(int64_t)totalBytesWritten
+totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        self.progressView.progress =   totalBytesWritten * 1.0/ totalBytesExpectedToWrite;
+
+    });
+    NSLog( @"%lld %lld %lld", bytesWritten, totalBytesWritten, totalBytesExpectedToWrite);
 }
 
 @end
